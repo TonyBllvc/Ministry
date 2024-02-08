9dbd958ff69dd747e817a555404ada0abf47bd92
TonyBllvc <george.ekwemadu@gmail.com>
2024-02-08 14:41:25 +0100

    update is remaining

--- routes/imageOne.js
+++ routes/imageOne.js
@@ -8,9 +8,11 @@ import path from "path";
 import Grid from 'gridfs-stream';
 import { gfs, store } from "../middleware/imageOne.js";
 import Image from "../model/image.js";
+import { GridFSBucket, MongoClient, ObjectId } from "mongodb";
 
 const router = express.Router()
 
+const baseUrl = ' http://localhost:4242/api/image/upload/'
 // let gfs
 // const conn = mongoose.createConnection('mongodb://127.0.0.1:27017/', {
 //     // useNewUrlParser: true,
@@ -117,44 +119,43 @@ router.route('/upload').post(store.single('image'), async (req, res) => {
 }).get(async (req, res) => {
     console.log('call')
     try {
-        let gft
+        const mongoClient = new MongoClient('mongodb://127.0.0.1:27017/', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoClient.connect();
+        console.log('1')
 
-        const conn = mongoose.createConnection('mongodb://127.0.0.1:27017/', {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-            // useCreateIndex: true
-        })
+        const database = mongoClient.db('test');
+        const images = database.collection('images.files');
 
-        conn.once('open', () => {
-            const mongo = mongoose.mongo; // Retrieve the mongo property from mongoose
-            gft = Grid(conn.db, mongo); // Provide mongo as the second argument
-            gft.collection('images.files')
-            console.log('connection')
-            // gfs = new mongoose.mongo.GridFSBucket(conn.db), {
-            //     bucketName: 'images'
-            // }
-        })
+        console.log('2')
         // Ensure that gft is initialized before using it
-        if (!gft) {
+        if (!images) {
             return res.status(500).json({ error: 'GridFS not initialized' });
         }
-        
-        // const image = Image.find 
-        const image = gft.find()
 
-        if ((await image.count()) == 0) {
-            return res.status(500).json({ error: 'No files found' })
+        console.log('3')
+        // const image = Image.find 
+        const cursor = images.find({})
+
+
+        console.log('5')
+
+        // Use collection.estimatedDocumentCount for approximate count
+        const estimatedCount = await images.estimatedDocumentCount();
+        if (estimatedCount === 0) {
+            return res.status(404).json({ error: 'No files found' });
         }
 
+        console.log('6')
+
         let fileInfo = []
-        await image.forEach((doc) => {
+        await cursor.forEach((doc) => {
             fileInfo.push({
                 name: doc.filename,
                 url: baseUrl + doc.filename
             })
         })
 
-        return res.status(200).json({ data: fileInfo, message: 'exist' })
+        return res.status(200).json({ data: fileInfo })
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
@@ -172,5 +173,84 @@ router.route('/upload').post(store.single('image'), async (req, res) => {
     //     res.status(200).json({ data: file, message: 'exist' })
     // })
 })
+
+router.route('/upload/:name').get(async (req, res) => {
+    console.log('call')
+    try {
+        const mongoClient = new MongoClient('mongodb://127.0.0.1:27017/', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoClient.connect();
+
+        const database = mongoClient.db('test');
+        const bucket = new GridFSBucket(database, {
+            bucketName: 'images'
+        })
+        // const images = database.collection('images.files');
+
+        console.log('2')
+        let downloadStream = bucket.openDownloadStreamByName(req.params.name);
+        // // Ensure that gft is initialized before using it
+        // if (!images) {
+        //     return res.status(500).json({ error: 'GridFS not initialized' });
+        // }
+
+        console.log('3')
+
+        downloadStream.on("data", function (data) {
+            return res.status(200).write(data);
+        });
+
+        downloadStream.on("error", function (err) {
+            return res.status(404).send({ message: "Cannot download the Image!" });
+        });
+
+        downloadStream.on("end", () => {
+            return res.end();
+        });
+
+        // return res.status(200).json({ data: fileInfo})
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+    // const image = gft.files.find()
+    // console.log(image)
+    // .toArray((err, files) => {
+    //     // Check if files exist
+    //     console.log('again')
+    //     if (!files || files.length == 0) {
+    //         console.log('once')
+    //         return res.status(404).json({ error: "No files found" })
+    //     }
+    //     console.log('and again')
+
+    //     res.status(200).json({ data: file, message: 'exist' })
+    // })
+})
+
+router.route('/upload/:id').delete(async (req, res) => {
+    try {
+        const mongoClient = new MongoClient('mongodb://127.0.0.1:27017/');
+        await mongoClient.connect();
+
+        const database = mongoClient.db('test'); // Adjust database name if needed
+        const bucket = new GridFSBucket(database, {
+            bucketName: 'images'
+        });
+
+        const imageCheck = await Image.findById(req.params.id)
+
+        if (!imageCheck) {
+            return res.status(404).json({ error: "Content not found" });
+        }
+        
+        await bucket.delete(new ObjectId(req.params.id));
+
+        await Image.findByIdAndDelete({ _id: req.params.id })
+        
+        res.status(200).json({ message: "File deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 
 export default router
